8f66fad23839979b63397913c1c52f2a4e9d9950
PhantomZ3 <sd.phantomz3@gmail.com>
2026-06-02 17:31:55 +0500

    Initial Commit

---
+++ src/client/java/com/drypted/mobblacklist/client/MobBlacklistClient.java
@@ -0,0 +1,10 @@
+package com.drypted.mobblacklist.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MobBlacklistClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
